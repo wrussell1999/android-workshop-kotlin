e38593e662d6c6bb4323e094edd0b876f28872a3
Will Russell <will@will-russell.com>
2020-01-17 03:46:09 +0000

    Added intents

--- app/src/main/java/com/willrussell/guessinggame/MainActivity.kt
+++ app/src/main/java/com/willrussell/guessinggame/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.willrussell.guessinggame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.TextView
 import kotlin.random.Random
@@ -20,6 +23,14 @@ class MainActivity : AppCompatActivity() {
         numberView = findViewById(R.id.number_view)
         inputEditText = findViewById(R.id.user_input)
         generateNumber()
+        
+        inputEditText!!.setOnEditorActionListener { v, actionId, event ->
+            if ((event?.keyCode == KeyEvent.KEYCODE_ENTER) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                checkGuess(v)
+                true
+            }
+            false
+        }
     }
 
     fun generateNumber() {
@@ -31,8 +42,9 @@ class MainActivity : AppCompatActivity() {
         val guessedValue = Integer.parseInt(input)
 
         if (guessedValue == randomNumber) {
-            numberView!!.text = resources.getString(R.string.guess_correct_label)
-            generateNumber()
+            val intent = Intent(this, PartyActivity::class.java)
+            startActivity(intent)
+
         } else {
             numberView!!.text = resources.getString(R.string.guess_incorrect_label)
         }
---
+++ app/src/main/java/com/willrussell/guessinggame/PartyActivity.kt
@@ -0,0 +1,17 @@
+package com.willrussell.guessinggame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class PartyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_party)
+    }
+
+    fun tryAgain(v: View) {
+        // return to say do it again
+    }
+}
